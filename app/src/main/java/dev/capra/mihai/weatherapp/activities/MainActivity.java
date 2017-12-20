8c6cbc6ce1afff0068df421dc226c470b93adb66
Mihai <mihai.capra27@gmail.com>
2017-12-20 15:24:46 +0200

    convert unixtimestamp into string value week day, convert string icon into image(load in view with Glide)

--- app/src/main/java/dev/capra/mihai/weatherapp/activities/MainActivity.java
+++ app/src/main/java/dev/capra/mihai/weatherapp/activities/MainActivity.java
@@ -3,11 +3,16 @@ package dev.capra.mihai.weatherapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.content.ContextCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.animation.AlphaAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.Locale;
 
 import dev.capra.mihai.weatherapp.R;
@@ -27,6 +32,10 @@ public class MainActivity extends BaseActivity {
     private TextView mMinMaxTemp;
     private TextView mWindSpeed;
     private TextView mWindDirection;
+    private ImageView mCurrentStatusImage;
+    private TextView mDay1, mDay2, mDay3, mDay4;
+    private ImageView mDay1Icon, mDay2Icon, mDay3Icon, mDay4Icon;
+    private TextView mDay1Temp, mDay2Temp, mDay3Temp, mDay4Temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +48,28 @@ public class MainActivity extends BaseActivity {
         mMinMaxTemp = findViewById(R.id.today_min_max_temp_txt);
         mWindSpeed = findViewById(R.id.wind_speed_txt);
         mWindDirection = findViewById(R.id.wind_direction);
+        mCurrentStatusImage = findViewById(R.id.today_icon);
+
+        LinearLayout linearLayout = findViewById(R.id.horizontal_layout_days);
+        //day1
+        mDay1 = linearLayout.findViewById(R.id.day1_txt);
+        mDay1Icon = linearLayout.findViewById(R.id.day1_icon);
+        mDay1Temp = linearLayout.findViewById(R.id.day1_temperature);
+
+        //day2
+        mDay2 = linearLayout.findViewById(R.id.day2_txt);
+        mDay2Icon = linearLayout.findViewById(R.id.day2_icon);
+        mDay2Temp = linearLayout.findViewById(R.id.day2_temperature);
+
+        //day3
+        mDay3 = linearLayout.findViewById(R.id.day3_txt);
+        mDay3Icon = linearLayout.findViewById(R.id.day3_icon);
+        mDay3Temp = linearLayout.findViewById(R.id.day3_temperature);
+
+        //day4
+        mDay4 = linearLayout.findViewById(R.id.day4_txt);
+        mDay4Icon = linearLayout.findViewById(R.id.day4_icon);
+        mDay4Temp = linearLayout.findViewById(R.id.day4_temperature);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(false);
@@ -87,27 +118,85 @@ public class MainActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void getWeatherData(){
+    private void getWeatherData() {
         Call<WeatherData> call = ApiClient.getOpenWeatherInterface().getWeatherData();
 
         call.enqueue(new Callback<WeatherData>() {
             @Override
             public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
-                mTemp.setText(String.format(Locale.US, "%d °C",response.body().getCurrentTemp()));
+
+                mTemp.setText(String.format(Locale.US, "%d °C", response.body().getCurrentTemp()));
                 mLoaction.setText(response.body().getCity());
                 mCurrentState.setText(response.body().getWeatherDescription());
                 mSunriseTime.setText(Utils.getHourMinutes(response.body().getSunrise()));
                 mSunsetTime.setText(Utils.getHourMinutes(response.body().getSunset()));
                 mMinMaxTemp.setText(String.format("%s...%s",
-                        response.body().getMinTemp(),response.body().getMaxTemp()));
-                mWindSpeed.setText(String.format(Locale.US,"%.1f m/s",response.body().getWindSpeed()));
+                        response.body().getMinTemp(), response.body().getMaxTemp()));
+                mWindSpeed.setText(String.format(Locale.US, "%.1f m/s", response.body().getWindSpeed()));
                 mWindDirection.setText(Utils.DegreesToCardinalDetailed(response.body().getWindDirection()));
+                Utils.loadImageWithGlide(MainActivity.this, mCurrentStatusImage, String.format("w_%s", response.body().getWeatherIcon()));
+
+                mDay1.setText(Utils.getWeekDay(response.body().getNextDays().get(0).getDay()));
+                Utils.loadImageWithGlide(MainActivity.this, mDay1Icon, String.format("w_%s", response.body().getNextDays().get(0).getIcon()));
+                mDay1Temp.setText(String.format(
+                        Locale.US,"%d...%d",
+                        response.body().getNextDays().get(0).getMinTemp().intValue(),
+                        response.body().getNextDays().get(0).getMaxTemp().intValue()));
+
+                mDay2.setText(Utils.getWeekDay(response.body().getNextDays().get(1).getDay()));
+                Utils.loadImageWithGlide(MainActivity.this, mDay2Icon, String.format("w_%s", response.body().getNextDays().get(1).getIcon()));
+                mDay2Temp.setText(String.format(
+                        Locale.US, "%d...%d",
+                        response.body().getNextDays().get(1).getMinTemp().intValue(),
+                        response.body().getNextDays().get(1).getMaxTemp().intValue()));
+
+                mDay3.setText(Utils.getWeekDay(response.body().getNextDays().get(2).getDay()));
+                Utils.loadImageWithGlide(MainActivity.this, mDay3Icon, String.format("w_%s", response.body().getNextDays().get(2).getIcon()));
+                mDay3Temp.setText(String.format(
+                        Locale.US, "%d...%d",
+                        response.body().getNextDays().get(2).getMinTemp().intValue(),
+                        response.body().getNextDays().get(2).getMaxTemp().intValue()));
+
+                mDay4.setText(Utils.getWeekDay(response.body().getNextDays().get(3).getDay()));
+                Utils.loadImageWithGlide(MainActivity.this, mDay4Icon, String.format("w_%s", response.body().getNextDays().get(3).getIcon()));
+                mDay4Temp.setText(String.format(
+                        Locale.US, "%d...%d",
+                        response.body().getNextDays().get(3).getMinTemp().intValue(),
+                        response.body().getNextDays().get(3).getMaxTemp().intValue()));
             }
 
             @Override
             public void onFailure(Call<WeatherData> call, Throwable t) {
-
+                Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void loadResponseInUi(Response<WeatherData> response) {
+        mTemp.setText(String.format(Locale.US, "%d °C", response.body().getCurrentTemp()));
+        mLoaction.setText(response.body().getCity());
+        mCurrentState.setText(response.body().getWeatherDescription());
+        mSunriseTime.setText(Utils.getHourMinutes(response.body().getSunrise()));
+        mSunsetTime.setText(Utils.getHourMinutes(response.body().getSunset()));
+        mMinMaxTemp.setText(String.format("%s...%s",
+                response.body().getMinTemp(), response.body().getMaxTemp()));
+        mWindSpeed.setText(String.format(Locale.US, "%.1f m/s", response.body().getWindSpeed()));
+        mWindDirection.setText(Utils.DegreesToCardinalDetailed(response.body().getWindDirection()));
+
+        mDay1.setText(response.body().getNextDays().get(1).getDay());
+        mDay1Icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.cloudyday));
+        mDay1Temp.setText(String.format("%s...%s", response.body().getNextDays().get(1).getMinTemp(), response.body().getNextDays().get(1).getMaxTemp()));
+
+        mDay2.setText(response.body().getNextDays().get(2).getDay());
+        mDay2Icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.cloudyday));
+        mDay2Temp.setText(String.format("%s...%s", response.body().getNextDays().get(2).getMinTemp(), response.body().getNextDays().get(2).getMaxTemp()));
+
+        mDay3.setText(response.body().getNextDays().get(3).getDay());
+        mDay3Icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.cloudyday));
+        mDay3Temp.setText(String.format("%s...%s", response.body().getNextDays().get(3).getMinTemp(), response.body().getNextDays().get(3).getMaxTemp()));
+
+        mDay4.setText(response.body().getNextDays().get(4).getDay());
+        mDay4Icon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.cloudyday));
+        mDay4Temp.setText(String.format("%s...%s", response.body().getNextDays().get(3).getMinTemp(), response.body().getNextDays().get(4).getMaxTemp()));
     }
 }
--- app/src/main/java/dev/capra/mihai/weatherapp/utils/Utils.java
+++ app/src/main/java/dev/capra/mihai/weatherapp/utils/Utils.java
@@ -1,6 +1,13 @@
 package dev.capra.mihai.weatherapp.utils;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -10,24 +17,41 @@ import java.util.TimeZone;
  */
 
 public class Utils {
-    public static String getHourMinutes(long unixTimeStamp){
+    public static String getHourMinutes(long unixTimeStamp) {
         Date time = new Date(unixTimeStamp * 1000L);
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm", Locale.US);
         simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT+2"));
         return simpleDateFormat.format(time);
     }
 
-    public static String DegreesToCardinal(double degrees)
-    {
-        String[] caridnals = { "N", "NE", "E", "SE", "S", "SW", "W", "NW", "N" };
-        return caridnals[(int)Math.round(((double)degrees % 360) / 45)];
+    public static String DegreesToCardinal(double degrees) {
+        String[] caridnals = {"N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"};
+        return caridnals[(int) Math.round((degrees % 360) / 45)];
     }
 
-    public static String DegreesToCardinalDetailed(double degrees)
-    {
+    public static String DegreesToCardinalDetailed(double degrees) {
         degrees *= 10;
 
-        String[] caridnals = { "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N" };
-        return caridnals[(int)Math.round(((double)degrees % 3600) / 225)];
+        String[] caridnals = {"N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N"};
+        return caridnals[(int) Math.round((degrees % 3600) / 225)];
+    }
+
+    private static String getStringWeekDay(int index) {
+        String[] days = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        return days[index-1];
+    }
+
+    public static String getWeekDay(long unixTimeStamp) {
+        Calendar date = Calendar.getInstance();
+        date.setTimeInMillis(unixTimeStamp * 1000);
+        return getStringWeekDay(date.get(Calendar.DAY_OF_WEEK));
+    }
+
+    public static void loadImageWithGlide(Context context, ImageView imageView, String name){
+        Glide.with(context).load(getImage(context, name)).into(imageView);
+    }
+
+    private static int getImage(Context context, String name){
+        return context.getResources().getIdentifier(name, "drawable", context.getPackageName());
     }
 }
